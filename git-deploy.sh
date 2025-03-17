f2f45a5dfc593c93cf479e8a455817facd1652fc
Seung Ki Lee <seungkilee.cs@gmail.com>
2025-03-17 14:25:37 +0900

    updated the deploy script-2025-03-17_14_25_21

--- git-deploy.sh
+++ git-deploy.sh
@@ -24,7 +24,7 @@ if [ -d .git ] || git rev-parse --git-dir > /dev/null 2>&1; then
     fi
 
     # OPTIONAL - Run custom script
-    npm run predeploy && npm run deploy
+    rm -rf dist && npm run build && npm run deploy
 
     # Add all changes to git
     git add .
