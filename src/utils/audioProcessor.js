b9409575d06a85cf9ebb31302311f48d2544f034
Seung Ki Lee <seungkilee.cs@gmail.com>
2025-03-31 15:18:01 +0900

    added spectogram computing logic 1. getting audio context with Web Audio API-2025-03-31_15_17_15

--- src/utils/audioProcessor.js
+++ src/utils/audioProcessor.js
@@ -15,3 +15,17 @@ function applyHannWindow(segment) {
   const hannWindow = generateHannWindow(N);
   return segment.map((value, index) => value * hannWindow[index]);
 }
+
+export async function computeSpectogram(file, fftSize = 512, overlap = 0.5) {
+  // audio file handling for
+  const audioContext = new AudioContext();
+  const arrayBuffer = await file.arrayBuffer();
+  const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+  // first channel
+  const chanelData = audioBuffer.gerChanelData(0);
+
+  // Spectogram Params
+  const spectogramData = [];
+
+  return spectogramData;
+}
