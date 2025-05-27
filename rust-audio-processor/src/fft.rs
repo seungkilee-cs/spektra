db97e8e374f99c37e39cc2e8f46161d395da4b9e
Seung Ki Lee <seungkilee.cs@gmail.com>
2025-05-27 09:53:21 +0900

    implemented rust fft and ifft-2025-05-27_09_53_05

--- rust-audio-processor/src/fft.rs
+++ rust-audio-processor/src/fft.rs
@@ -2,20 +2,20 @@
 use crate::utils::{bit_reverse, Complex, generate_twiddle_factor, butterfly_operation};
 
 pub fn fft(input: &mut [Complex]) {
-    // input validation
+    // input validations
     let n = input.len();
-    assert!(n.is_power_of_two(), "input size must be power of 2");
+    assert!(n.is_power_of_two(), "Input size must be power of 2");
 
     // bit reversal permutation
     let bits = n.trailing_zeros() as usize;
     for i in 0..n {
         let j = bit_reverse(i, bits);
         if i < j {
-            input.swap(i,j);
+            input.swap(i, j);
         }
     }
-    
-    // Iterative, Radix 2, In place Cooley-Tukey
+
+    // Inplace, Radix 2 Iterative Cooley-Tukey
     let mut len = 2;
     while len <= n {
         let half = len / 2;
@@ -32,5 +32,22 @@ pub fn fft(input: &mut [Complex]) {
             }
         }
         len *= 2;
+    }
+}
+
+pub fn ifft(input: &mute [Complex]) {
+    let n = input.len();
+    // Conjugate All Inputs
+    for c in input.iter_mut() {
+        c.imag = -c.imag;
+    }
+
+    // Forward fft
+    fft(input);
+
+    // Conjugate Again and Scale
+    for c in input.iter_mut() {
+        c.real /= n as f32;
+        c.imag = -c.imag / n as f32;
     }
 }
